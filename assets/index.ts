d8741d2c65a8af11c5a48ea7afc9ab7328c31739
kieuntd <125887753+kieuntd132@users.noreply.github.com>
2023-07-14 01:26:08 +0700

    assest

--- assets/index.ts
+++ assets/index.ts
@@ -1,11 +1,16 @@
 export const BACKGROUND = require('./Pepsi_Boy_RGB_bg_1.png');
-export const ICON_HOME = require('./Icon-Home.png');
+export const ICON_HOME = require('./Icon_Home.png');
 export const LOGO_PEPSI = require('./Logo-Pepsi.png');
 export const BACKGROUND_BOOTOM_TAB = require('./Pepsi_Boy_RGB_03 1.png');
-export const TEXT_PEPSI_MUSIC = require('./Text.png');
-export const TALEN = require('./talen.png');
+export const TEXT_PEPSI_MUSIC = require('./Pepsi_Music.png');
+export const TALENT = require('./talent.png');
 export const HEADLINE_2 = require('./HEADLINE_2.png');
 export const LEFT_BUTTON = require('./LeftButton.png');
 export const RIGHT_BUTTON = require('./RightButton.png');
 export const CENTER_BUTTON = require('./CenterButton.png');
+export const ICON_LITLEMICRO = require('./Icon_litleMicro.png');
+export const ICON_MICROPHONE = require('./Icon_microphone.png');
+export const ICON_PEPSI = require('./Icon_pepso.png');
+export const LEFT_BUTTON_HOME = require('./LeftButton_Home.png');
+export const RIGHT_BUTTON_HOME = require('./RightButton_Home.png');
 
